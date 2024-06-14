b83560cd9ac462e1b1d810a3f4520e02c57aa60a
MozesSchepens <mozes.schepens@student.ehb.be>
2024-06-14 14:05:30 +0200

    klaar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,32 +1,45 @@
 @extends('layouts.app')
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-</head>
+@section('content')
+<style>
+     body {
+        font-family: Arial, sans-serif;
+        background-color: white;
+        overflow-x: hidden;
+    }
+    .aboutcontent{
+        width: 80%;
+        justify-content: space-around;
+    }
+    h1{
+        color: #001838;
+    }
+    strong, h5{
+        color: navy;
+    }
+    p{
+        color: #001838;
+    }
+    
+</style>
 <body>
-    <header>
-        <h1>About This Project</h1>
-    </header>
-    <main>
-        <>
-            <h2>Project beschrijving</h2>
-            <>Ik was al begonnen aan deze taak zonder github. Heb zojuist al mijn files en codes geupload 
+    <div class="container">
+       
+        <h1>Project beschrijving</h1>
+        <hr>
+            <p>Ik was al begonnen aan deze taak zonder github. Heb zojuist al mijn files en codes geupload 
                 Mijn messages heb ik niet echt over nagedacht ik wou het zo snel mogelijk hierop zetten dat dit in orde zou zijn.
                 Om dit project te kunnen maken heb ik gebruik gemaakt van het internet heb ook verschillende keren gevraagd aan medestudenten of ze mij konden helpen. 
                 Ik heb een site gemaakt over mijn favoriete voetbalploeg namelijk Manchester City. Ik heb hier mij gebaseerd op de layout van de officiele club site en op verschillende fan sites 
                 (omdat ik zelf niet zo goed ben in style). Hierin vind je een news page, players page, shop page, faq page en een contact page. Er zijn een aantal zaken dat niet helemaal werken 
                 ik zoals login soms werkt het en soms niet en ik begrijp niet zo goed hoe dat komt.
             </p>
+            <p>Hier is de link naar github: <a href="https://github.com/MozesSchepens/project-laravel.git">https://github.com/MozesSchepens/project-laravel.git</p> 
         </section>
-    </main>
     <footer>
         <p>&copy; 2024 Mozes Schepens. All rights reserved.</p>
     </footer>
+        </div>
+   
 </body>
 </html>
+@endsection
